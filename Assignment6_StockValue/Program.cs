9315c48b5b816ef7ce14963469854d1fea0fbc4f
Gopu <ganeshsivasai.gopu@dxc.com>
2023-04-27 12:08:59 +0530

    intial commit

---
+++ Assignment6_StockValue/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Assignment6_StockValue
+{
+    class Furniture
+    {
+        public int Height { get; set; }
+        public int Width { get; set; }
+        public string Color { get; set; }
+        public int Quantity { get; set; }
+        public int Price { get; set; }
+
+        public void Acceptdetails()
+        {
+            Console.WriteLine("Enter the Height");
+            int h = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Width");
+            int w = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the color");
+            string c = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Enter the Quantity");
+            int q = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+    class BookShelf : Furniture
+    {
+        public int NoOfShelves { get; set; }
+    }
+    class DiningTable : Furniture
+    {
+        public int NoOfLegs { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
